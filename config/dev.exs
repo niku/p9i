dc39b5115419b1ed91b4611210ba3674c51e1b0d
niku <niku@niku.name>
2017-10-19 20:03:41 +0900

    Integrate `tsc --watch` with `mix phx.server`

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,9 @@ config :p9i, P9iWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: []
+  watchers: [
+    node: ["node_modules/typescript/bin/tsc", "--watch", "--pretty"]
+  ]
 
 # ## SSL Support
 #
