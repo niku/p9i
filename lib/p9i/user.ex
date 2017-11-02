75fce0e304cd2663dee83e5d6c97b545aa6431a5
niku <niku@niku.name>
2017-11-02 20:32:04 +0900

    Create User model
    Main perpose of this commit is making `priv/repo/migrations` directory to avoid error when `mix ecto.migrate` executes.
    So, It doesn't have any additional column.
    
    $ mix phx.gen.schema User users

---
+++ lib/p9i/user.ex
@@ -0,0 +1,20 @@
+defmodule P9i.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias P9i.User
+
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "users" do
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%User{} = user, attrs) do
+    user
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
